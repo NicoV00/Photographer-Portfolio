bd625b3ca73a106851a89302f4fbe5bb9c472fe3
JOTALGS <114532448+JOTALGS@users.noreply.github.com>
2024-09-25 20:19:55 +0000

    offcanvas

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -1,29 +1,69 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const OffCanvas = ({ name, ...props }) => {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-  
+
     return (
       <>
-        <button style={{width:'20px', color:"black"}} variant="primary" onClick={handleShow} className="me-2">
+        <button style={{width:'100px', backgroundColor:"black"}} onClick={handleShow} className="me-2">
           {name}
         </button>
-        <Offcanvas show={show} onHide={handleClose} placement="end" name="sabi" {...props}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            Some text as placeholder. In real life you can have the elements you
-            have chosen. Like, text, images, lists, etc.
-          </Offcanvas.Body>
-        </Offcanvas>
+        {show && (
+          <div style={styles.overlay}>
+            <div style={styles.canvas}>
+              <div style={styles.header}>
+                <h2>Offcanvas</h2>
+                <button onClick={handleClose} style={styles.closeButton}>X</button>
+              </div>
+              <div style={styles.body}>
+                Some text as placeholder. In real life you can have the elements you
+                have chosen. Like, text, images, lists, etc.
+              </div>
+            </div>
+          </div>
+        )}
       </>
     );
+};
+
+const styles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'rgba(1, 1, 1, 0.8)',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  canvas: {
+    width: '900px',
+    height: '100%',
+    color: 'white',
+    backgroundColor: 'black',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+    padding: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  closeButton: {
+    backgroundColor: 'transparent',
+    border: 'none',
+    fontSize: '20px',
+    cursor: 'pointer',
+  },
+  body: {
+    marginTop: '20px',
+  },
 };
 
 export default OffCanvas;
