1d47f8ca0bc1ecbd400f43740839d5106282c4ac
NicoV00 <6328@holbertonstudents.com>
2025-08-05 17:34:40 -0300

    added speed from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { gsap } from 'gsap';
 import * as THREE from 'three';
 import MyWaySection from './components/MyWaySection';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Lazy loaded galleries
 const AnaLivniGallery = lazy(() => import('./components/Galleries/AnaLivniGallery'));
@@ -630,6 +631,7 @@ function App() {
         />
       </ContainerCloud>
       
+       <SpeedInsights />
       {/* Vercel Analytics */}
       <Analytics />
     </>
